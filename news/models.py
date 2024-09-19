58ce732541b848c5b903ff416242ca93e921c2e7
Jonny Broughton <jonny.broughton08@gmail.com>
2024-09-19 22:05:44 +0000

    add post model

--- news/models.py
+++ news/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=150, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="news_posts")
+    content = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    STATUS = ((0, "Draft"), (1, "Published"))
